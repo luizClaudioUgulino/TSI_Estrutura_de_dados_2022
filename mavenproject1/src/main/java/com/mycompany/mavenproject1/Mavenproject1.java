2336967c3b9ad3cb7ed6a9ffa6be82927fe0ccca
Luiz Claudio Ugulino <77798329+luizClaudioUgulino@users.noreply.github.com>
2022-11-20 22:14:43 -0300

    aula02
    Estrutura de dados

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author lcunha
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        
+		System.out.println("raiz de 64: "+Math.pow(64, 0.5));
+		
+		System.out.println("raiz com exp e log e realint: "+(int)Math.pow(64,0.5));
+		
+		System.out.println("raiz com exp e log sem realint: "+Math.pow(64,0.5));
+		
+		System.out.println("formatar "+Math.sin(45*3.14/180));
+		
+		System.out.println("potencia com operador ** e formatar: "+Math.pow(8,3.3));
+		
+		System.out.println("raiz cubica: "+Math.pow(8,0.3333));
+		
+		System.out.println("absoluto "+Math.abs(-8));
+		
+		System.out.println("absoluto "+Math.abs(8));
+		
+		System.out.println("convertendo para real: "+(double)87);
+		
+		System.out.println("convertendo para inteiro 5.5: "+(int)5.5);
+		
+		System.out.println("convertendo para inteiro 6.5: "+(int)6.5);
+		
+		System.out.println("convertendo para inteiro 6.5+0.0001: "+(int)6.5+0.0001);
+		
+		System.out.println("convertendo para inteiro 5.45: "+(int)5.45);
+		
+		System.out.println("convertendo para inteiro 5.51: "+(int)5.51);
+		
+		System.out.println("convertendo para inteiro:"+(int)3/4);
+    }
+}
